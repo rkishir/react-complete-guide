b2323930f29474a9244c9042776fb16cf1b354c0
Rakesh Kapoor <rkapoor@ishir.com>
2020-06-21 20:49:08 +0530

    Using hooks to mange state in functional component.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons:[
-      {name:"Rakesh",age:"34"},
-      {name:"guragam",age:"3"},
-      {name:"lovenet",age:"33"}
-    ],
-    otherState: "checking"
-  }
-//Event handling
-switchNameHandler = ()=>{
-  console.log('clicked');
-  this.setState({
-    persons:[
-      {name:"kapoor",age:"40"},
-      {name:"guragam",age:"3"},
-      {name:"lovenet",age:"39"}
-    ]
-  })
-}
+const App = props => {
 
-  render() {
-    return (
-      <div className="App">
-       
-       <h1> Welcome to React</h1>
-       <p>working</p>
-       <button onClick={this.switchNameHandler}>Switch</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-       
-      </div>
-    );
-    // return React.createElement('div',{className:"App"}, React.createElement('h1',null,'Hi i am a react'));
+  var [PersonsState, setPersonsState] = useState({
+      persons: [
+        { name: "Rakesh", age: "34" },
+        { name: "guragam", age: "3" },
+        { name: "lovenet", age: "33" }
+      ]
+    });
+
+  // Eligent way of merging two state
+  const [otherState,setOtherState] = useState('any data you wan t to add to state');
+
+  console.log(PersonsState,otherState);
+    //Event handling
+ const switchNameHandler = () => {
+    console.log('clicked');
+    setPersonsState({
+      persons: [
+        { name: "kapoor", age: "40" },
+        { name: "guragam", age: "3" },
+        { name: "lovenet", age: "39" }
+      ]
+    })
   }
+
+
+  return (
+    <div className="App">
+
+      <h1> Welcome to React</h1>
+      <p>working</p>
+      <button onClick={switchNameHandler}>Switch</button>
+      <Person name={PersonsState.persons[0].name} age={PersonsState.persons[0].age} />
+      <Person name={PersonsState.persons[1].name} age={PersonsState.persons[1].age} />
+      <Person name={PersonsState.persons[2].name} age={PersonsState.persons[2].age} />
+
+    </div>
+  );
+
 }
 
 export default App;
+
